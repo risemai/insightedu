ebb1b49f911ab38aee3f5d37891e008eb0c39a3f
Imran <mahmud.bubt.150@gmail.com>
2025-12-30 19:32:27 +0600

    add lenis smooth scroll

--- components/layouts/default-layout.tsx
+++ components/layouts/default-layout.tsx
@@ -3,10 +3,20 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { PublicFooter, PublicNavbar } from '../shared';
 import { getQueryClient } from '@/lib/react-query';
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 
 const queryClient = getQueryClient();
 
 export function DefaultLayout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <PublicNavbar />
